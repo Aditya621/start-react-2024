b854ccbbf7a9a48ad5ef3c5bc2dd287af0347359
Aditya Singh <56670647+Aditya621@users.noreply.github.com>
2024-04-15 11:02:41 +0000

    Make button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,88 @@
 import { useState } from "react";
 import "./App.css";
 
+const products = [
+  { id: 1, title: "Monitor", recommend: false },
+  { id: 2, title: "Chair", recommend: false },
+  { id: 3, title: "Mouse", recommend: true },
+];
+
 function MyButton() {
-  return <button>I'm button</button>;
+  function handleEvent() {
+    alert("button Clicked");
+  }
+
+  return <button onClick={handleEvent}>I'm button</button>;
 }
 
-function App() {
+function Counter() {
   const [count, setCount] = useState(0);
+
+  function handleCount() {
+    setCount((count) => count + 1);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
 
   return (
     <>
-      <MyButton />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div>
+        <button
+          style={{
+            margin: "10px",
+            backgroundColor: "black",
+            color: "white",
+            borderRadius: "10px",
+            padding: "10px",
+          }}
+          onClick={handleCount}
+        >
+          Count was {count}
+        </button>
+        <button className="btn" onClick={resetCount}>
+          Reset Count
         </button>
       </div>
+    </>
+  );
+}
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  // function resetCount() {
+  //   setCount(0);
+  // }
+
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{ color: product.recommend ? "green" : "red" }}
+      onClick={() => alert(product.title)}
+    >
+      {product.title}
+    </li>
+  ));
+
+  return (
+    <>
+      <ul>{listItems}</ul>
+      <MyButton />
+      <Counter />
+      {/* <div className="card">
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+          }}
+        >
+          count is {count}
+        </button>
+        <button style={{ margin: "10px" }} onClick={resetCount}>
+          Reset Count
+        </button>
+      </div> */}
     </>
   );
 }
